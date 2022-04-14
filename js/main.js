dac58cbeae2f63b0c9c2788a22fdff8b1fea0a85
Eradzh Mirzoev <eradzh.m@gmail.com>
2022-04-14 12:03:12 +0300

    HW-DOM-part-3

---
+++ js/main.js
@@ -0,0 +1,25 @@
+let input = document.querySelector('input');
+let ul = document.querySelector('ul');
+let addList = document.querySelector('.addList');
+
+document.querySelector('.wrapper').addEventListener('click', (event) => {
+    let elemLi = document.createElement('li');
+
+    if(event.target.className === 'addList' && input.value !== '') {
+        elemLi.innerHTML =`${input.value} <button class="delete">DELETE</button>`;
+        input.value = '';
+        ul.append(elemLi);
+    }
+
+    if(event.target.className === 'delete') {
+        event.target.closest('li').remove();
+    }
+    
+    if(event.target.closest('li')) {
+        event.target.closest('li').classList.toggle('changeBg');
+    }
+
+    console.log(event.target.className)
+
+})
+
